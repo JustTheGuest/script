3f78e262ecc76c4c4e5a60931c65a5461240fed0
JustTheGuest <memberxd710@gmail.com>
2025-03-17 09:47:53 +0200

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,13 +1,9 @@
-print("Script module loading")
-local function esp()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/JustTheGuest/script/refs/heads/main/mm.lua'))()
-end
-
-
-task.spawn(esp)
-
-
 setclipboard("Public on top! Telegram: https://t.me/JospakHome / Discord: https://discord.gg/cS6qcnBStG")
+
+print("Script module loading")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/JustTheGuest/script/refs/heads/main/mm.lua'))()
+
+
 
 print("GUI")
 loadstring(game:HttpGet('https://raw.githubusercontent.com/JustTheGuest/script/refs/heads/main/gui.lua'))()
